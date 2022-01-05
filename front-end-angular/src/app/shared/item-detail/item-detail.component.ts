d8a874e76fad4a7e11f46bcd7d5f03b12bd2ae95
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 18:43:12 -0300

    Se arma onDestroy

--- front-end-angular/src/app/shared/item-detail/item-detail.component.ts
+++ front-end-angular/src/app/shared/item-detail/item-detail.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ItemModel } from 'src/app/models/item-model';
+import { ItemsService } from 'src/app/services/items-service.service';
 
 @Component({
-  selector: 'app-item-detail',
+  selector: 'item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss']
 })
-export class ItemDetailComponent implements OnInit {
+export class ItemDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() idItem: string;
+  
+  item: ItemModel = {};
+  error: boolean = false;
+  
+  subscribes: Subscription[] = [];
+
+
+  constructor(private itemsData: ItemsService) { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+		this.subscribes.forEach(s => s.unsubscribe());
+	}
+
+ 
 }
