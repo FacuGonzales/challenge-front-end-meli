6a8ee94334df4c06202134afd54cf5832e27457f
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-04 22:24:58 -0300

    Agregamos cabeceras y cors

--- api/app.js
+++ api/app.js
@@ -7,5 +7,14 @@ app.use(EXPRESS.urlencoded({extended: true}))
 app.use(EXPRESS.static('public'))
 app.use(EXPRESS.json())
 
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', ' Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 
 module.exports = app;
