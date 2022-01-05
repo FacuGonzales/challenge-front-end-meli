d94cbaa7c9179fd327caead6debcdc80e380f107
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 15:31:25 -0300

    Se agrega librerias de form

--- front-end-angular/src/app/shared/shared-components.module.ts
+++ front-end-angular/src/app/shared/shared-components.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
          
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [
-    NavBarComponent
+    NavBarComponent,
   ],
   providers: [],
   exports: [
-    NavBarComponent
+    NavBarComponent,
   ]
 })
 
