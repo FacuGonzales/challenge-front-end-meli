c30670e6fe69446d025f9ca6e7fbd06d05d2d154
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 18:01:41 -0300

    Se arma on Destroy

--- front-end-angular/src/app/components/home-component/page/home.component.ts
+++ front-end-angular/src/app/components/home-component/page/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  params: string = '';
+  subscribes: Subscription[] = [];
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.initParams();
   }
+
+  ngOnDestroy() {
+		this.subscribes.forEach(s => s.unsubscribe());
+	}
 
 }
