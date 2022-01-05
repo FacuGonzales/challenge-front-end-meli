fc7d1467fb27274e52dba5275384682750980e20
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 14:27:16 -0300

    Se crea module para Shareds Components

---
+++ front-end-angular/src/app/shared/shared-components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+         
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    NavBarComponent
+  ],
+  providers: [],
+  exports: [
+  ]
+})
+
+export class SharedComponentsModule { }
