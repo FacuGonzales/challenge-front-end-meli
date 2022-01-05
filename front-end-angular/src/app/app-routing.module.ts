0f02f1a4585565f7289b9cae881e75a022688506
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 14:21:09 -0300

    Se arma ruta de home

--- front-end-angular/src/app/app-routing.module.ts
+++ front-end-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const defaultPathApp = '/';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./components/home-component/home.module').then(m => m.HomeModule), 
+  },
+  {
+    path: '**', redirectTo: defaultPathApp
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
