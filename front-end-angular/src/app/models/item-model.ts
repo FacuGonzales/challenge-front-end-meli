b838de03a8fd9e8f13e27883b7281563df28778f
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-05 17:44:26 -0300

    Se crea modelo de datos para items

---
+++ front-end-angular/src/app/models/item-model.ts
@@ -0,0 +1,16 @@
+import { AddressModel } from "./address-model";
+import { ShippingModel } from "./shipping-model";
+
+export interface ItemModel {
+    id?: string,
+    title?: string,
+    price?: string,
+    sale_price?: string,
+    available_quantity?: number,
+    currency_id?: number,
+    img?: string[],
+    condition?: string,
+    shipping?: ShippingModel,
+    address?: AddressModel,
+    description?: string,
+}
