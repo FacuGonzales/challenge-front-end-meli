37a30309823438ae4698f16b1d71d10aace8533f
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-06 13:30:19 -0300

    Se actualiza shared module

--- front-end-angular/src/app/shared/shared-components.module.ts
+++ front-end-angular/src/app/shared/shared-components.module.ts
@@ -4,6 +4,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ItemsListComponent } from './items-list/items-list.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { TableItemsComponent } from './table-items/table-items.component';
 
          
 
@@ -17,12 +18,14 @@ import { ItemDetailComponent } from './item-detail/item-detail.component';
     NavBarComponent,
     ItemsListComponent,
     ItemDetailComponent,
+    TableItemsComponent,
   ],
   providers: [],
   exports: [
     NavBarComponent,
     ItemsListComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
+    TableItemsComponent
   ]
 })
 
