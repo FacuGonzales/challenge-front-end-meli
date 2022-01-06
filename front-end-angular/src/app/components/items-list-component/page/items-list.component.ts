727053aa5ab8506adaf2233916e4d5733a7679d7
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-06 15:56:33 -0300

    Se arma logica para obtener listado

--- front-end-angular/src/app/components/items-list-component/page/items-list.component.ts
+++ front-end-angular/src/app/components/items-list-component/page/items-list.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { ItemsService } from 'src/app/services/items-service.service';
+import { Subscription } from 'rxjs';
+import { ItemModel } from 'src/app/models/item-model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-items-list',
   templateUrl: './items-list.component.html',
   styleUrls: ['./items-list.component.scss']
 })
-export class ItemsListComponent implements OnInit {
+export class ItemsListComponent implements OnInit,OnDestroy {
+  
+  params: string = '';
+  itemsList: ItemModel[] = [];
+  error: boolean = false;
 
-  constructor() { }
+  subscribes: Subscription[] = [];
+
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private itemsData: ItemsService) { }
 
   ngOnInit(): void {
+    this.initParams();
+    if(this.params) this.getItems();
   }
 
+
+  ngOnDestroy() {
+		this.subscribes.forEach(s => s.unsubscribe());
+	}
+
+  initParams(){
+		this.subscribes[0] = this.route.params.subscribe(params => this.params = params['search'] ? params['search']  : '');
+	}
+
+  getItems(){
+    this.subscribes[0] = this.itemsData.getListItems(this.params).subscribe(
+      result => {
+        if(!result) this.error = true;
+
+        this.itemsList = result;
+      },err => console.error(err)
+    )
+  }
 }
