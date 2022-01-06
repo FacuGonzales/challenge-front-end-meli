56d0939b988ef03f963b07b468dc785c6874c92f
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-06 13:30:09 -0300

    Se crea componente para tabla de items

---
+++ front-end-angular/src/app/shared/table-items/table-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-items',
+  templateUrl: './table-items.component.html',
+  styleUrls: ['./table-items.component.scss']
+})
+export class TableItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
