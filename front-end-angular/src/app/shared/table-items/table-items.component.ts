3ace707d4a3c11aa8b1eee6ec3ce15a1cf384d20
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-06 16:00:32 -0300

    Se arma logica de tabla

--- front-end-angular/src/app/shared/table-items/table-items.component.ts
+++ front-end-angular/src/app/shared/table-items/table-items.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { ItemModel } from 'src/app/models/item-model';
 
 @Component({
-  selector: 'app-table-items',
+  selector: 'table-items',
   templateUrl: './table-items.component.html',
   styleUrls: ['./table-items.component.scss']
 })
 export class TableItemsComponent implements OnInit {
 
-  constructor() { }
+  @Input() data: ItemModel[] = [];
+  itemsList: ItemModel[] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data && this.data.length) {
+      this.completTable();
+    }
+  }
+
+  completTable(){
+    this.itemsList = this.data
+  }
+
+  viewDetail(id){
+    this.router.navigate([`/item/${id}`])
+  }
 }
