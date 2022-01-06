fcb9132208f3e31284e7c783980813cabb223e0b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-06 15:56:12 -0300

    Se arma logica de home

--- front-end-angular/src/app/components/home-component/page/home.component.ts
+++ front-end-angular/src/app/components/home-component/page/home.component.ts
@@ -1,35 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit{ //OnDestroy {
 
-  params: string = '';
-  id: string = '';
-  subscribes: Subscription[] = [];
-
-  constructor(private router: Router,
-              private route: ActivatedRoute,) { }
+  constructor(){}
 
   ngOnInit(): void {
-    this.initParams();
-  }
-
-  ngOnDestroy() {
-		this.subscribes.forEach(s => s.unsubscribe());
-	}
-
-  initParams(){
-		this.subscribes[0] = this.route.params.subscribe(params => this.params = params['search'] ? params['search']  : '');
-    this.subscribes[1] = this.route.params.subscribe(params => this.id = params['id'] ? params['id']  : '');
-	}
-
-  searchValue(value:string){
-    this.router.navigate([`/items/${value.toLowerCase()}`])
   }
 }
