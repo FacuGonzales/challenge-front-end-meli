47abdc3bd326ffa4a7eec11bf6360b02bc692446
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-08 15:30:46 -0300

    Se actualiza shared

--- front-end-angular/src/app/shared/shared-components.module.ts
+++ front-end-angular/src/app/shared/shared-components.module.ts
@@ -6,7 +6,8 @@ import { DetailItemComponent } from './detail-item/detail-item.component';
 import { ItemComponent } from './item/item.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { InputSearchComponent } from './input-search/input-search.component';
-import { PriceComponent } from './price/price.component';     
+import { PriceComponent } from './price/price.component';
+import { ErrorMessageComponent } from './error-message/error-message.component';     
 
 @NgModule({
   imports: [
@@ -21,6 +22,7 @@ import { PriceComponent } from './price/price.component';
     SearchBarComponent,
     InputSearchComponent,
     PriceComponent,
+    ErrorMessageComponent,
   ],
   providers: [],
   exports: [
@@ -28,7 +30,8 @@ import { PriceComponent } from './price/price.component';
     DetailItemComponent,
     SearchBarComponent,
     InputSearchComponent,
-    PriceComponent
+    PriceComponent,
+    ErrorMessageComponent
   ]
 })
 
