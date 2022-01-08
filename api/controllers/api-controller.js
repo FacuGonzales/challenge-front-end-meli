d3ad6862084e898f3cfda00e3b5e77f0e0356402
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-01-08 16:52:25 -0300

    Se modifica metodos de api

--- api/controllers/api-controller.js
+++ api/controllers/api-controller.js
@@ -6,86 +6,72 @@ function prueba(req, res) {
 }
 
 async function getForSearch(req, res) {
-    try {
-        const response = await AXIOS({
-            method: 'get',
-            url: 'https://api.mercadolibre.com/sites/MLA/search?q='+req.params.search,
-            responseType: 'json'
-        });
+    const {search} = req.params;
 
-        let {results} = response.data;
-        let _newList = new Array();
+    try{
+        AXIOS.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}`)
+        .then(response => {
+            let {results} = response.data;
+            let _newList = new Array();
 
-        results.slice(0, 4).forEach(element => {
+            results.slice(0, 4).forEach(element => {
 
-            let _item = new Item();
+                let _item = new Item();
+    
+                _item.id = element.id,
+                _item.title = element.title,
+                _item.price = element.price,
+                _item.sale_price = element.sale_price,
+                _item.available_quantity = element.available_quantity,
+                _item.sold_quantity = element.sold_quantity,
+                _item.currency_id = element.currency_id,
+                _item.img = element.thumbnail,
+                _item.condition = element.condition,
+                _item.shipping = element.shipping,
+                _item.address = element.address,
+                _item.description = element.description
+    
+                _newList.push(_item);
+            });
+    
+            res.send(_newList);
 
-            _item.id = element.id,
-            _item.title = element.title,
-            _item.price = element.price,
-            _item.sale_price = element.sale_price,
-            _item.available_quantity = element.available_quantity,
-            _item.sold_quantity = element.sold_quantity,
-            _item.currency_id = element.currency_id,
-            _item.img = element.thumbnail,
-            _item.condition = element.condition,
-            _item.shipping = element.shipping,
-            _item.address = element.address,
-            _item.description = element.description
-
-            _newList.push(_item);
-        });
-
-        res.send(_newList);
-
-        // return productos;
+        }).catch(error => res.status(error.response.status).send({message:  error.response.data.message}));
     } catch (error) {
-        console.error(error);
+        return res.status(error.response.status).send({message:  error.response.data.message})
     }
-
 }
 
 async function getForId(req, res){
+    const {id} = req.params;
+
+    try{
+        await AXIOS.all([
+            AXIOS.get(`https://api.mercadolibre.com/items/${id}`),
+            AXIOS.get(`https://api.mercadolibre.com/items/${id}/description`)
+        ])
+        .then(responseArr => {
+            let {data} = responseArr[0];
+            let {plain_text} = responseArr[1].data;
+            
+            let _itemDetail = new Item();
+            _itemDetail.id = data.id,
+            _itemDetail.title = data.title,
+            _itemDetail.price = data.price,
+            _itemDetail.sale_price = data.sale_price,
+            _itemDetail.available_quantity = data.available_quantity,
+            _itemDetail.sold_quantity = data.sold_quantity,
+            _itemDetail.currency_id = data.currency_id,
+            _itemDetail.img = data.pictures.map( i => i.url);
+            _itemDetail.condition = data.condition,
+            _itemDetail.shipping = data.shipping,
+            _itemDetail.address = data.address;
+            _itemDetail.description = plain_text
+            res.send(_itemDetail);
     
-    try {
-        const responseItem = await AXIOS({
-            method: 'get',
-            url: `https://api.mercadolibre.com/items/${req.params.id}`,
-            responseType: 'json'
-        });
-        
-        let {data} = responseItem
-
-        console.log(data);
-
-        const responseDescription = await AXIOS({
-            method: 'get',
-            url: `https://api.mercadolibre.com/items/${req.params.id}/description`,
-            responseType: 'json'
-        });
-
-        let {plain_text} = responseDescription.data;
-
-        let _item = new Item();
-
-        _item.id = data.id,
-        _item.title = data.title,
-        _item.price = data.price,
-        _item.sale_price = data.sale_price,
-        _item.available_quantity = data.available_quantity,
-        _item.sold_quantity = data.sold_quantity,
-        _item.currency_id = data.currency_id,
-        _item.img = data.pictures.map( i => i.url);
-        _item.condition = data.condition,
-        _item.shipping = data.shipping,
-        _item.address = data.address,
-        _item.description = plain_text
-
-        res.send(_item);
-
-        // return producto;
+        }).catch(error => res.status(error.response.status).send({message:  error.response.data.message}));
     } catch (error) {
-        console.error(error);
+        return res.status(error.response.status).send({message:  error.response.data.message})
     }
 }
 
